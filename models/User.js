ae92203e5014e10e7e3f4a8e5d8ec689e69bda1c
JoshuaSowerby <josh.d.sowerby@gmail.com>
2025-04-02 11:23:27 +1000

    template userSchema

--- models/User.js
+++ models/User.js
@@ -1,1 +1,16 @@
 // should store unser data like settings, and username...
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Auth", required: true },///fix this in the router section
+    userName: {
+        type: String
+    },
+    setting1:{
+        Boolean: true
+    }
+});
+
+module.exports = mongoose.model("User", userSchema);

b532ae12fcdbcd850af7b25fff60072d8455eae1
JoshuaSowerby <josh.d.sowerby@gmail.com>
2025-04-09 09:44:46 +1000

    fixed incorrect ref and added username to scoreboard

--- models/Leaderboard.js
+++ models/Leaderboard.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const leaderboardSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User", required: true },///fix this in the router section
+        ref: "Auth", required: true },///fix this in the router section
+    userName:{
+        type: String,
+        ref: "User"
+    },
     score: {
         type: Number, // should it be a reference to most recent score in score history
         required: true}
